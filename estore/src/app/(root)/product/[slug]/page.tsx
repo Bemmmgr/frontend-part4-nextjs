343301b6a30bc91103c9bae6736c5bd50d9c8069
Xiangyu Li <lix0230@gmail.com>
2026-06-13 16:42:09 +0200

    026 - product images component, finish section3

--- estore/src/app/(root)/product/[slug]/page.tsx
+++ estore/src/app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
+import ProductImages from "@/components/shared/product/product-image";
 
 // 025 - Product details page
 const ProductDetailPage = async (props: {
@@ -20,7 +21,9 @@ const ProductDetailPage = async (props: {
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5">
           {/* Images column */}
-          <div className="col-span-2">{/* Images components */}</div>
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
           <div className="col-span-2 p-5">
             <div className="flex flex-col gap-6">
               <p>
