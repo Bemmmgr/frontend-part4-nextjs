c57fa682b0437525588c1be82a1c0d7f2af524c0
Xiangyu Li <lix0230@gmail.com>
2026-06-19 18:18:27 +0200

    cart: zod shcema & prisma model 045

--- estore/src/types/index.ts
+++ estore/src/types/index.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
-import { insetProductsSchema } from "@/lib/validators";
+import {
+  insetProductsSchema,
+  insertCartSchema,
+  cartItemSchema,
+} from "@/lib/validators";
 
 // 023 - zod calidation & type inference
 export type Product = z.infer<typeof insetProductsSchema> & {
@@ -7,3 +11,7 @@ export type Product = z.infer<typeof insetProductsSchema> & {
   rating: string;
   createdAt: Date;
 };
+
+// 045 - infer types of cart
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
