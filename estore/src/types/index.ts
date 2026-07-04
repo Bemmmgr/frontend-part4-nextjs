8dbe2ccfe19a0d04043417f4ead3f0784fc957a9
Xiangyu Li <lix0230@gmail.com>
2026-07-04 14:43:44 +0200

    059 - order/orderItem zod schema & infer types

--- estore/src/types/index.ts
+++ estore/src/types/index.ts
@@ -4,6 +4,8 @@ import {
   insertCartSchema,
   cartItemSchema,
   shippingAddressSchema,
+  insertOrderSchema,
+  insertOrderItemSchema,
 } from "@/lib/validators";
 
 // 023 - zod calidation & type inference
@@ -17,3 +19,19 @@ export type Product = z.infer<typeof insetProductsSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
+// 059 - infer types of order & orderItems
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDeilvered: boolean;
+  deilveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: {
+    name: string;
+    email: string;
+  };
+};
