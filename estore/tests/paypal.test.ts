97469235192d81e7c2d6fdfd475904be89b6f45f
Xiangyu Li <lix0230@gmail.com>
2026-07-08 06:40:54 +0200

    080/081 - create order, capture payment request, Jest testing for order payment

--- estore/tests/paypal.test.ts
+++ estore/tests/paypal.test.ts
@@ -1,5 +1,6 @@
-import { expect, test } from "@jest/globals";
-import { generateAccessToken } from "@/lib/paypal";
+import { expect, jest, test } from "@jest/globals";
+import { generateAccessToken, paypal } from "@/lib/paypal";
+import { mock } from "node:test";
 
 // 079 - jest testing for access token
 // test to generate access token from paypal
@@ -7,4 +8,31 @@ test("generates token from paypal", async () => {
   const tokenResponse = await generateAccessToken();
   expect(typeof tokenResponse).toBe("string");
   expect(tokenResponse.length).toBeGreaterThan(0);
+});
+
+// 081 - test to create a paypal order
+test("creates a paypal order", async () => {
+  const price = 10.0;
+
+  const orderRes = await paypal.createOrder(price);
+  console.log(orderRes);
+
+  expect(orderRes).toHaveProperty("id");
+  expect(orderRes.status).toBe("CREATED");
+});
+
+// test to capture payment with mock order
+test("simulate capyuring a payment from an order", async () => {
+  const orderId = "101";
+
+  const mockCapturePayment = jest
+    .spyOn(paypal, "capturePayment")
+    .mockResolvedValue({
+      status: "COMPLETED",
+    });
+
+  const captureRes = await paypal.capturePayment(orderId);
+  expect(captureRes).toHaveProperty("status", "COMPLETED");
+
+  mockCapturePayment.mockRestore();
 });
