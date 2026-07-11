e244e4f243ab63f4edb8cc66fdc71c06a45e576f
Xiangyu Li <lix0230@gmail.com>
2026-07-11 06:39:03 +0200

    087 - get my orders action

--- estore/src/lib/constants/index.ts
+++ estore/src/lib/constants/index.ts
@@ -39,3 +39,6 @@ export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   : ["PayPal", "Stripe", "CashOnDelivery"];
 export const DEFAULT_PAYMENT_METHOD =
   process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
+
+// 087 - get my orders action
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2;
